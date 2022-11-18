824d7dd6d99d867bcc8c76cf8324b55d8fdda207
Victor Bonanho <contato@victorbonanho.com>
2022-11-17 22:15:17 -0300

    first commit com form rodando

---
+++ LinfoBack/Model/Social.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinfoBack.Model
+{
+    class Social
+    {
+        public string instagram { get; set; }
+        public string linkedin { get; set; }
+        public string github { get; set; }
+        public string whatsapp { get; set; }
+        public string email { get; set; }
+    }
+}
---
+++ LinfoBack/Model/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinfoBack.Model
+{
+    class User
+    {
+        public string nome { get; set; }
+        public string sobrnome { get; set; }
+        public string password { get; set; }        
+        public string email { get; set; }
+        public string phone { get; set; }
+    }
+}
